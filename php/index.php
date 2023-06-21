db05982a2051cd536bf24267e8539b027bfe1819
Nicolas Bureš <nick.bures@gmail.com>
2023-06-21 10:55:45 +0200

    set up app and connection to database

---
+++ php/index.php
@@ -0,0 +1,19 @@
+<?php
+    $db_host = "localhost";
+    $db_user = "root";
+    $db_password = "";
+    $db_name = "products-management";
+
+    $connection = mysqli_connect($db_host, $db_user, $db_password, $db_name);
+
+
+    if (mysqli_connect_error()){
+        echo mysqli_connect_error();
+        exit;
+    }
+
+
+
+echo "Úspěšné přihlášení do databáze";
+
+?>  
